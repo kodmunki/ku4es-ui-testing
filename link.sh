ee18f647297c26b38fa7a081322d099dce1844db
kodmunki <kodmunki@gmail.com>
2017-08-11 08:46:01 -0500

    update

--- link.sh
+++ link.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd ${BASE_DIR}
 rm -rf ./node_modules/ku4es-kernel
 mkdir ./node_modules/ku4es-kernel
 cp -rf ../ku4es-kernel/dist/. ./node_modules/ku4es-kernel
+cd -
